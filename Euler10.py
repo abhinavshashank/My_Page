4e11760cd8353c17cdc4cf002371a65376b41e4b
Abhinav Shashank Choudhary <abhinavshashank008@gmail.com>
2018-08-28 23:10:07 +0530

    Create Euler10.py

---
+++ Euler10.py
@@ -0,0 +1,17 @@
+'''The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+Find the sum of all the primes below two million.
+
+Value=142913828922'''
+
+def isprime(num):
+    for i in range(2, int(num/2)+1):
+        if(num%i==0):
+            return 1
+    return 0
+n=10
+sum=0
+for i in range (2,n):
+    if(isprime(i)==0):
+        print(sum)
+        sum+=i
+print(sum)
