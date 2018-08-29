6fdc7cf91f6829a47753567145453a89d62a8d83
Abhinav Shashank Choudhary <abhinavshashank008@gmail.com>
2018-08-29 11:03:52 +0530

    Update Euler010.py

--- Euler010.py
+++ Euler010.py
@@ -8,10 +8,9 @@ def isprime(num):
         if(num%i==0):
             return 1
     return 0
-n=10
+n=2000000
 sum=0
 for i in range (2,n):
     if(isprime(i)==0):
-        print(sum)
-        sum+=i
+        um+=i
 print(sum)
